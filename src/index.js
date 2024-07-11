3b806570229569dccebf1d4e8e98b09602356de1
Patryk Majchrzak <pmaj12345@gmail.com>
2024-07-11 18:28:54 +0200

    GlobalStyle added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { GlobalStyle } from './Globalstyles';
+import { GlobalStyle } from './GlobalStyles';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 
