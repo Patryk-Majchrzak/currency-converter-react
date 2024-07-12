b105d20aaf98d2ca530c8e5ca42e1ce4a2733759
Patryk Majchrzak <pmaj12345@gmail.com>
2024-07-12 16:56:32 +0200

    removing currencies.js since I got currencies from API

--- src/App/currencies.js
+++
@@ -1,8 +0,0 @@
-
-export const currencies =
-[
-  { id: 1, short: "PLN", rateToEUR: 0.2308, rateToGBP: 0.1950, rateToUSD: 0.2467 },
-  { id: 2, short: "EUR", rateToPLN: 4.3331, rateToGBP: 0.8448, rateToUSD: 1.0692 },
-  { id: 3, short: "GBP", rateToPLN: 5.1291, rateToEUR: 1.1837, rateToUSD: 1.2656 },
-  { id: 4, short: "USD", rateToPLN: 4.0527, rateToEUR: 0.9353, rateToUSD: 0.7901 },
-];
