0157f32275c7a5a2535edf3a6f41d3f7039138a0
Patryk Majchrzak <pmaj12345@gmail.com>
2024-07-15 16:48:19 +0200

    changed GlobalStyles location

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { GlobalStyle } from './GlobalStyles';
+import { GlobalStyle } from '../src/GlobalStyles';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 
